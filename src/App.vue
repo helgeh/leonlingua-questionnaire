8f5c84b60382d6b94aece8deaa896cea97e0b5aa
Helge Hofstad <helge.hofstad@gmail.com>
2023-08-05 17:33:47 +0200

    change color scheme for buttons and links

--- src/App.vue
+++ src/App.vue
@@ -35,6 +35,13 @@
 </script>
 
 <style>
+    :root {
+        --bs-link-color: #319E00;
+        --bs-link-color-rgb: 49,101,69;
+        --bs-link-decoration: underline;
+        --bs-link-hover-color: #30b700;
+        --bs-link-hover-color-rgb: 48,104,74;
+    }
     #app {
         font-family: "Mulish", Mulish, sans-serif;
         -webkit-font-smoothing: antialiased;
@@ -42,4 +49,21 @@
         color: #2c3e50;
         margin-top: 60px;
     }
+    .btn-primary {
+        --bs-btn-color: #fff;
+        --bs-btn-bg: #319E00;
+        --bs-btn-border-color: #319E00;
+        --bs-btn-hover-color: #fff;
+        --bs-btn-hover-bg: #30b700;
+        --bs-btn-hover-border-color: #207203;
+        --bs-btn-focus-shadow-rgb: 49,132,253;
+        --bs-btn-active-color: #fff;
+        --bs-btn-active-bg: #207203;
+        --bs-btn-active-border-color: #238500;
+        --bs-btn-active-shadow: inset 0 3px 5px rgba(0, 0, 0, 0.125);
+        --bs-btn-disabled-color: #fff;
+        --bs-btn-disabled-bg: #319E00;
+        --bs-btn-disabled-border-color: #319E00;
+    }
+
 </style>
